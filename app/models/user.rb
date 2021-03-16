98bc8c45c6799dc8f52081cec1a8630512d971bd
uimarshall <uimarshall@gmail.com>
2021-03-16 21:55:21 +0100

    Add associations for friends and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :friends, class_name: 'Friend', foreign_key: 'reference_id'
 end
