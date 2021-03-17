8109069a5f1e2829076e3b49f01537d63716a252
uimarshall <uimarshall@gmail.com>
2021-03-17 21:01:56 +0100

    Refactor code

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friends
   # get 'home/index'
-  root 'home#index'
+  # root 'home#index'
+  root 'friends#index'
   get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
